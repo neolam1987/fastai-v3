03087f9bd7835cc586f89ca339049a32ab1a0a33
neolam1987 <61340583+neolam1987@users.noreply.github.com>
2020-04-26 23:21:23 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1zkCfitUXRPbmkupi-6IMuuw8gpD9pj6_'
 export_file_name = 'Tree_class_Branch_R50_85%.pkl'
 
-classes = ['Ficus hispida','Acacia confusa','Lophostemon confertus','Macaranga tanarius','Machilus spp','Schefflera heptaphylla']
+classes = ['FH','AC','LC','MH','MS','SH']
 path = Path(__file__).parent
 
 app = Starlette()
